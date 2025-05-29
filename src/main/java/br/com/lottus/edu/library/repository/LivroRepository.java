06f7d276f49af922bed9bd151d2c8913f1bd4a40
nathannolacio <nathannolacio04@gmail.com>
2025-05-29 20:18:16 -0300

    :recycle: refactor: Refatorado a query do metodo de busca de Livro

--- src/main/java/br/com/lottus/edu/library/repository/LivroRepository.java
+++ src/main/java/br/com/lottus/edu/library/repository/LivroRepository.java
@@ -13,21 +13,28 @@ import java.util.List;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
-   @Query(value = "SELECT new br.com.lottus.edu.library.dto.LivroResponseDTO(" +
-           "  l.id, l.nome, l.autor, l.quantidade, l.quantidadeDisponivel, l.status, c.nome, l.descricao) " +
-           "FROM Livro l JOIN l.categoria c " +
-           "WHERE (:termoBusca IS NULL OR " +
-           "      LOWER(l.nome) LIKE LOWER(CONCAT('%', :termoBusca, '%')) OR " +
-           "      LOWER(l.autor) LIKE LOWER(CONCAT('%', :termoBusca, '%'))) " +
-           "AND (:status IS NULL OR l.status IN :status) " + // Corrigido aqui, não era :statusList
-           "AND (:categoriaId IS NULL OR c.id = :categoriaId)",
-           countQuery = "SELECT COUNT(l.id) " + // Ou COUNT(l)
-                   "FROM Livro l JOIN l.categoria c " +
-                   "WHERE (:termoBusca IS NULL OR " +
-                   "      LOWER(l.nome) LIKE LOWER(CONCAT('%', :termoBusca, '%')) OR " +
-                   "      LOWER(l.autor) LIKE LOWER(CONCAT('%', :termoBusca, '%'))) " +
-                   "AND (:status IS NULL OR l.status IN :status) " + // Corrigido aqui
-                   "AND (:categoriaId IS NULL OR c.id = :categoriaId)")
+   String SELECT = """
+    SELECT new br.com.lottus.edu.library.dto.LivroResponseDTO(
+      l.id, l.nome, l.autor, l.quantidade, l.quantidadeDisponivel, l.status, c.nome, l.descricao)
+    FROM Livro l JOIN l.categoria c
+    WHERE (:termoBusca IS NULL OR
+          LOWER(l.nome) LIKE LOWER(CONCAT('%', :termoBusca, '%')) OR
+          LOWER(l.autor) LIKE LOWER(CONCAT('%', :termoBusca, '%')))
+    AND (:status IS NULL OR l.status IN :status)
+    AND (:categoriaId IS NULL OR c.id = :categoriaId)
+    """;
+
+   String COUNT = """
+    SELECT COUNT(l.id)
+    FROM Livro l JOIN l.categoria c
+    WHERE (:termoBusca IS NULL OR
+          LOWER(l.nome) LIKE LOWER(CONCAT('%', :termoBusca, '%')) OR
+          LOWER(l.autor) LIKE LOWER(CONCAT('%', :termoBusca, '%')))
+    AND (:status IS NULL OR l.status IN :status)
+    AND (:categoriaId IS NULL OR c.id = :categoriaId)
+    """;
+
+   @Query(value = SELECT, countQuery = COUNT)
    Page<LivroResponseDTO> findByBuscaOuFiltro(
            @Param("termoBusca")String valor,
            @Param("status") List<StatusLivro> status,
